7452dc30d1b54a1e752cc89edefa8a7f98e4eeac
Jeadine29 <143826069+Jeadine29@users.noreply.github.com>
2024-06-19 06:11:45 +0800

    update package json

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -1,7 +1,6 @@
 // src/components/Notification.js
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from 'styles.module.css';
 
 const Notification = ({ message }) => {
   return <p>{message}</p>;
